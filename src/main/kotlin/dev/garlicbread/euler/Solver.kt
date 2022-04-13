e2409385b14563cdfdd33e162e16fdb93367a53b
tiemonl <tiemon.liam@gmail.com>
2022-04-13 14:43:45 -0400

    NOGH: update solver to show times

--- src/main/kotlin/dev/garlicbread/euler/Solver.kt
+++ src/main/kotlin/dev/garlicbread/euler/Solver.kt
@@ -2,6 +2,7 @@ package dev.garlicbread.euler
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.system.measureNanoTime
 
 fun <T : Any> solve(block: () -> Problem<T>) {
     printResult(block)
@@ -15,12 +16,27 @@ fun getInput(problem: Int): List<String>? {
         ?.let { Files.readAllLines(it) }
 }
 
-fun <T : Any> printResult(block: () -> Problem<T>) {
-    val problem = block()
-    println("Problem ${problem.problem}".bold())
-    val problemResult = problem.solveProblem().bold("36")
+fun printResult(block: () -> Problem<*>) {
+    lateinit var problem: Problem<*>
+    lateinit var problemResult: String
+    var problemInitTime = 0L
+    var problemTime = 0L
+    var problemTotalTime = 0L
+
+    problemInitTime += measureNanoTime { problem = block() }
+    problemTime += measureNanoTime {
+        problemResult = problem.solveProblem()!!.bold("36")
+    }
+    problemTotalTime += problemInitTime + problemTime
+
+    println("Problem ${problem.problem}".bold("32"))
     println("Result: $problemResult")
+    println("Init Time: ${"%.3fms".format(problemInitTime.toMillis()).bold("33")}")
+    println("Problem Time: ${"%.3fms".format(problemTime.toMillis()).bold("33")}")
+    println("Total Time: ${"%.3fms".format(problemTotalTime.toMillis()).bold("33")}")
 }
+
+private fun Long.toMillis() = this / 1000000f
 
 private fun Any.bold(color: String = ""): String = style("$color;1")
 
